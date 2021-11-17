af12ecf297eb9130035c97e385b64fa96f17ceb5
ekemper <eddie@videoblocks.com>
2021-11-17 15:34:06 -0700

    added return val

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,12 @@
 
 exports.handler = async function(event, context) {
-    
+
     console.log('test')
 
     console.log({event, context})
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
 }
